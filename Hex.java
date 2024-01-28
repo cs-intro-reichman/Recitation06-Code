c265e6424e71e6f08ab31ef70ba07936bc2838b1
Tal Danai <70094025+danaital@users.noreply.github.com>
2024-01-28 10:34:56 +0200

    Update Hex.java

--- Hex.java
+++ Hex.java
@@ -1,18 +1,18 @@
 public class Hex {
     public static void main (String [] args) {
-         System.out.println(decToHex(1071)); // output: “42F”  
+        System.out.println(decToHex(1071)); // output: “42F”  
     }
     public static String decToHex(int num) {
         if (num == 0){
-       return "0";
+            return "0";
         }
         int cur = num;
         String hexDigits = "0123456789ABCDEF";
         String hex = "";
         while (cur > 0) {
-       int rem = cur % 16;
-       hex = hexDigits.charAt(rem) + hex;
-       cur /= 16; 
+            int rem = cur % 16;
+            hex = hexDigits.charAt(rem) + hex;
+            cur /= 16; 
         }
        return hex;
    }
