3be960d2936270cf9012f9b2960edff421f7b0a9
Tal Danai <70094025+danaital@users.noreply.github.com>
2024-01-28 10:34:41 +0200

    Update ExceptionExamples.java

--- ExceptionExamples.java
+++ ExceptionExamples.java
@@ -1,18 +1,33 @@
 public class ExceptionExamples {
+    public static void main(String[] args) {
+        int answer = readInt3();
+        System.out.println("the number read is " + answer);
+    }
   /**
    * Tries to read an integer through the standard input
    * If an illegal argument is entered, returns 0
    */
   public static int readInt() {
-    In in = new In ();
+    In in = new In();
     int ans = 0;
     StdOut.println("Please enter a number.");
     try {
-      String s = in.readString();
-      ans = Integer.parseInt(s);
+        String s = in.readString();
+        // s = s.charAt(-1) + s;
+        ans = Integer.parseInt(s);
+        ans = 1/ans;
+        System.out.println("Reached end of try");
     } catch (NumberFormatException e) {
-      ans = 0;
+        System.out.println("Reached catch 1");
+        ans = 0;
+    } catch (ArithmeticException e) {
+        System.out.println("Reached catch 2");
+        ans = 0;
     }
+    // } catch (StringIndexOutOfBoundsException e) {
+        // System.out.println("Reached catch 3");
+
+    // }
     return ans;
   }
 
@@ -47,13 +62,18 @@ public class ExceptionExamples {
       In in = new In ();
       try {
         String s = in.readString();
+        System.out.println("started try");
         ans = Integer.parseInt(s);
+        System.out.println("Reached end of try");
       } catch (NumberFormatException e) {
+        System.out.println("Reached catch");
         ans = 0;
       } finally {
+        System.out.println("Reached finally");
           // will execute always.
-          return ans;
       }
+      return ans;
+
   }
   
 
